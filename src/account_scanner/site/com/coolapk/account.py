9f2e6fee074d018c39b81fda5336f86d9f4951bf
LiHua-Official <lihua114514@pm.me>
2021-05-29 18:24:47 +0800

    commit

---
+++ src/account_scanner/site/com/coolapk/account.py
@@ -0,0 +1,62 @@
+url='https://account.coolapk.com/auth/findPassword'
+header={
+        'Host': 'account.coolapk.com',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+        'Accept': '*/*',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'x-requested-with': 'XMLHttpRequest',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://account.coolapk.com',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Referer': 'https://account.coolapk.com/auth/findPassword',
+        'Sec-GPC': '1',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache'
+        }
+data={
+        'submit': '1',
+        'requestHash': 'eed9aaecqtv57y',
+        'country': '86',
+        'mobile': '18888888888',
+        'captcha': '',
+        'randomNumber': '0undefined035251207877570856'
+        }
+false = {
+        "themeName":"",
+        "charset":"UTF-8",
+        "requestApp":"AccountV8",
+        "baseUrl":"",
+        "requestBase":"",
+        "requestBaseUrl":"",
+        "requestHash":"58f0bb24qtv58g",
+        "requestTime":1622282560,
+        "ajaxRequest":true,
+        "requestAjax":true,
+        "SESSION":{
+            "uid":0,
+            "username":null,
+            "status":0,
+            "isLogin":false,
+            "isMember":false,
+            "isAdministrator":false,
+            "isSuperAdministrator":false,
+            "isTester":false
+            },
+        "appName":"AccountV8",
+        "className":"Apps\\AccountV8\\Controller\\Auth",
+        "controllerName":"Auth",
+        "calledMethodName":"onFindPassword",
+        "forward":"http:\/\/developer.coolapk.com",
+        "displayMode":"web",
+        "iosLogin":false,
+        "isCommunity":false,
+        "status":0,
+        "messageStatus":0,
+        "error":0,
+        "message":"\u8be5\u624b\u673a\u53f7\u672a\u7ed1\u5b9a\u9177\u5b89\u8d26\u53f7"
+        }
+true = {
+
+        }
